e19fe9acf6bf5719a0fc00ad89fd60396505cec6
yuriko-aya <thechildoflily@gmail.com>
2020-01-23 21:58:55 +0700

    [IMP] Finish the things

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# odoo-backup
-Odoo backup script
+# Odoo Backup
+
+Autobackup script for Odoo  
+Backup Odoo Database and Filestore
+
+# Requirement
+
+- `gsutil` from Google Cloud SDK for Google Cloud Storage and configured by root user
+- `aws` from aws-cli for Amazon Web Service S3
+- `mail` from mailutils
+
+# Feature
+
+Create 14 regular backups. 7 (seven) daily backups for a week, 4 (four) weekly backups for a month, and 3 (three) monthly backups for 3 months.
+
+## Storage Save
+
+This script won't backup if the space of the backup destination is less than 20% available, even if using cloud only backup. This is safety measure if the backup is so big that will cause system halt because of disk full
+
+## Three backup types
+
+### Full sync `backup_type="sync"`
+
+Full sync will save **ALL** 14 backups both on site and cloud storage. All backup action (create, and delete) will be done only on local machine, and then sent to the cloud using sync for AWS S3 and rsync for GCS.
+
+### Partial sync `backup_type="partial"`
+
+Partial sync will save all 14 backups on the cloud **BUT** only few on site. The `local_age` variable define how old on site backup is kept before deleted. This can be used when the disk space is limited but stll want to use on site backups.
+
+### Cloud only backup `backup_type="cloud"`
+Cloud only backup won't create any on site backup. All 14 backups will only be saved on the cloud. Once today's backup is sent to the cloud, on site back up will be deleted. This is for server with very limited disk available.
+
+## Mail Notification
+
+Setup mail notification for sendgrid is available using sendgrid send api
+
+## Logging
+
+Log is saved in `/var/log/backup/backup_script.log`
+
+# Supported Storage Provider
+
+- Google Cloud Storage 'gs'
+- Amazon Web Service S3 's3'
--- backup.sh
+++ backup.sh
@@ -1,86 +1,337 @@
 #!/bin/bash
 
-fs_backup_location=""
-db_backup_location=""
-s3_endpoint=" https://s3.cloud.cmctelecom.vn"
-s3_bucket="s3://phongcachxanh.vn-backups"
+## PATH
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:$PATH
 
-### Send email or error function
+################################################################################## Logging
+# create logfile if not exist
+logfile="/var/log/backup/backup_script.log"
+if [ ! -e $logfile ]
+then
+    mkdir -p "/var/log/backup/"
+    touch $logfile
+fi
+
+## add timestamp to log
+add_date() {
+    while IFS='' read -r line
+    do
+        echo "$(date): $line" >> $logfile
+    done
+}
+
+exec 1> >(add_date) 2>&1
+
+############################################################################# Send email on error function
 # usage send_errormail "message"
 send_errormail () {
     # curl https://api.sendgrid.com/api/mail.send.json \
-    # -F to= -F toname=test -F subject="Autobackup failed" \
+    # -F to= -F toname= -F subject="Autobackup failed" \
     # -F text="$1" \
     # -F from= -F api_user= -F api_key=
 
-    mail -s "Autobackup Failed!" alice@sanusi.id < $1
+    echo $1 | mail -s "Autobackup Failed!" root@localhost
 }
 
-### Cheking disk space
+############################################################################ Bakcup Variables
+### Local location variable
+db_name="odoo13"
+odoo_data_dir="/opt/odoo/.local/share/Odoo" # must be same with data_dir in odoo config
+fs_location="$odoo_data_dir/filestore/$db_name"
+backup_location="/opt/new-backup"
+mkdir -p $backup_location
+
+# Storage provider, "s3" for AWS S3 or "gs" for Google Cloud Storage 
+storage_provider="gs"
+
+## AWS S3 Config
+s3_endpoint="https://"
+s3_bucket="s3://"
+
+## Google Cloud Storage Config
+gs_bucket="gs://"
+
+### Advanced config
+## Backup type:
+# sync (default): full backup on local (7 daily, 4 weekly, and 3 monthly) and sync it to cloud
+# partial: full backup on the cloud but only few on local depends to local_age variable
+# cloud: cloud only backup, no local backup.
+backup_type="sync"
+local_age="4"       # in days, required only if backup_type="partial"
+
+############################################################################# Precheck for available disk space and Storage Provider
+# Abort backup if disk space less than 20%
 # usage: check_disk folder
 # return number is percent disk usage
 check_disk () {
-    disk_usage=$(df $1 | grep "/" | awk {'print $5'} | sed -e 's|%||'  )
+    disk_usage="$(df $1 | grep "/" | awk {'print $5'} | sed -e 's|%||'  )"
     echo $disk_usage
 }
 
-### Backup Function
-# usage: backup_function "type(db or fs)" "backup_destination(folder)" "backup_source(folder or db name if db type)" 
-backup_function () {
-    # Check disk, abort if disk morethan 80% used
-    # comment this if want disable disk check
-    disk_usage="$(check_disk $2)"
-    if [[ $disk_usage -ge 81 ]]
-    then
-        send_errormail "Disk space warning!\nDisk space usage is more than 80%! Backup aborted! Please make more space!"
-        exit 1
-    fi
+# Check disk, abort if disk morethan 80% used
+# comment this if want disable disk check
+disk_usage="$(check_disk $backup_location)"
+if [[ $disk_usage -ge 81 ]]
+then
+    echo "Disk space warning!\nDisk space usage is more than 80%! Backup aborted! Please make more space!"
+    send_errormail "Disk space warning!\nDisk space usage is more than 80%! Backup aborted! Please make more space!"
+    exit 1
+fi
 
-    # if backup type Database
-    if [ $1 == "db" ]
-    then
-        dbname="$3_$(date +%Y-%m-%d).gz"
-        check_db=$(sudo -u postgres psql -l | grep "$3")
-        if [ -z $check_db]
+## Check if provider is known
+if [ "$storage_provider" != "gs" ] && [ "$storage_provider" != "s3" ]
+then
+    echo "Provider not known! Aborting!"
+    exit 1
+fi
+
+################################################################################## Functions for backup 
+
+### Delete old backup on AWS S3
+# usage: delete_s3_old_backup folder to cleanup age(days)
+# example: delete_s3_old_backup db/daily 30
+delete_s3_old_backup() {
+    aws s3 --endpoint-url $s3_endpoint ls $s3_bucket/$1/ | grep " DIR " -v | while read -r line;
+    do
+        create_date=$(echo $line | awk {'print $1" "$2'})
+        create_date=$(date -d "$create_date" +%s)
+        older_than=$(date -d "$2 day ago" +%s)
+        if [[ $create_date -lt $older_than ]]
         then
-            send_errormail "Backup failed, database $3 not found!!"
-            return 1
-        else
-            sudo -u postgre pg_dump $3 | gzip > $2/$dbname
-            if [ $? -ne 0 ]
+            file_name=`echo $line | awk {'print $4'}`
+            if [[ $file_name != "" ]]
             then
-                send_errormail "Backup failed! Failed to backup database $3"
-                return 1
+                printf 'Deleting "%s"\n' $file_name
+                aws s3 --endpoint-url $s3_endpoint rm $s3_bucket/$1/$file_name
             fi
         fi
-    elif [ $1 == "fs"]
-    then
-        dirname=$(basename $3)
-        fsname="$dirname-$(date +%Y-%m-%d).tar.gz"
-        sudo tar zcf $2/fsname $3
-        if [ $? -ne 0]
+    done
+}
+
+### Delete old backup on Google Cloud Storage
+# usage: delete_gs_old_backup( folder to cleanup age(days)
+# example: delete_gs_old_backup db/daily 30
+delete_gs_old_backup() {
+    gsutil ls -l $gs_bucket/$1 | grep "TOTAL" -v | while read -r line;
+    do
+        create_date=$(echo $line | awk {'print $2'})
+        create_date=$(date -d "$create_date" +%s)
+        older_than=$(date -d "$2 day ago" +%s)
+        if [[ $create_date -lt $older_than ]]
         then
+            file_name=`echo $line | awk {'print $3'}`
+            if [[ $file_name != "" ]]
+            then
+                printf 'Deleting "%s"\n' $file_name
+                gsutil rm $file_name
+            fi
+        fi
+    done 
+}
+
+### Delete old backup on Google Cloud Storage
+# usage: delete_gs_old_backup( folder to cleanup age(days)
+# example: delete_local_old_backup /opt/backup/db/weekly 30
+delete_local_old_backup() {
+    echo "Deleting $2 days old backup from $1"
+    find "$1" -type f -mtime +$2 -delete
+}
+
+## Upload compressed backup to cloud function
+# usage cloud_upload file destination_object(db/daily, db/monthly, etc) age_to_delete
+cloud_upload() {
+    if [ "$storage_provider" = "gs" ]
+    then
+        echo "Uploading $1 to $gs_bucket/$2/"
+        gsutil cp $1 $gs_bucket/$2/
+        if [[ $? -ne 0 ]]
+        then
+            echo "Failed to upload backup to Google Cloud Storage\nFile: $1\nDestination: $2"
+            send_errormail "Failed to upload backup to Google Cloud Storage\nFile: $1\nDestination: $2"
+            return 1
+        fi
+        echo "Deleting $3 days old backup from $gs_bucket/$2"
+        delete_gs_old_backup $2 $3
+    elif [ "$storage_provider" = 's3' ]
+    then
+        echo "Uploading $1 to $s3_bucket/$2"
+        aws s3 --endpoint-url $s3_endpoint cp $1 $s3_bucket/$2/
+        if [[ $? -ne 0 ]]
+        then
+            echo "Failed to upload to AWS S3\nFile: $1\nDestination: $2"
+            send_errormail "Failed to upload to AWS S3\nFile: $1\nDestination: $2"
+            return 1
+        fi
+        echo "Deleting $3 days old backup from $s3_bucket/$2"
+        delete_s3_old_backup $2 $3
+    fi
+}
+
+cloud_sync() {
+    if [ "$storage_provider" = "gs" ]
+    then
+        echo "Syncronize $backup_location/$1 to $gs_bucket/$1/"
+        gsutil -m rsync -d -r $backup_location/$1 $gs_bucket/$1/
+        if [[ $? -ne 0 ]]
+        then
+            echo "Failed syncronize $backup_location/$1 to Google Cloud Storage"
+            send_errormail "Failed syncronize $backup_location/$1 to Google Cloud Storage"
+            return 1
+        fi
+    elif [ "$storage_provider" = 's3' ]
+    then
+        echo "syncronize $backup_location/$1 to $s3_bucket/$1"
+        aws s3 --endpoint-url $s3_endpoint sync $backup_location/$1 $s3_bucket/$1/
+        if [[ $? -ne 0 ]]
+        then
+            echo "Failed to syncronize $backup_location to AWS S3"
+            send_errormail "Failed to syncronize $backup_location to AWS S3"
+            return 1
+        fi
+    fi
+}
+
+### Backup Function
+# usage: backup_function "type(db or fs)" "backup_location(folder)" "backup_source(folder or db name if db type)" 
+backup_function () {
+    # if backup type Database
+    today_day=$(date +%A)
+    today_date=$(date +%d)
+    if [ "$1" = "db" ]
+    then
+        dbname="$3_$(date +%Y-%m-%d).gz"
+        echo "Backuping databse $dbname to $2"
+        sudo -u postgres pg_dump $3 | gzip > $2/$dbname
+        if [ $? -ne 0 ]
+        then
+            echo "Backup failed! Failed to backup database $3"
+            send_errormail "Backup failed! Failed to backup database $3"
+            return 1
+        fi
+        if [ "$backup_type" = "sync" ]
+        then
+            echo "Saving backup to daily backup directory"
+            mv "$2/$dbname" "$2/db/daily/"
+            echo "Deleting 7 days old backup from daily backup"
+            delete_local_old_backup "$2/db/daily/" 7
+            if [ "$today_day" = "Monday" ]
+            then
+                echo "Saving backup to weekly backup directory"
+                cp "$2/db/daily/$dbname" "$2/db/weekly/"
+                echo "Deleteing 30 days old backup from weekly backup directory"
+                delete_local_old_backup "$2/db/weekly/" 30
+            fi
+            if [ "$today_date" = "28" ]
+            then
+                echo "Saving backup to monthly backup directory"
+                cp "$2/db/daily/$dbname" "$2/db/monthly/"
+                echo "Deleting 90 days old backup from monthly backup directory"
+                delete_local_old_backup "$2/db/monthly/"
+            fi
+            cloud_sync "db"
+        else
+            cloud_upload "$2/$dbname" "db/daily" 6
+            if [ "$today_day" = "Monday" ]
+            then
+                cloud_upload "$2/$dbname" "db/weekly" 29
+            fi
+            if [ "$today_date" = "28" ]
+            then
+                cloud_upload "$2/$dbname" "db/monthly" 89
+            fi
+            if [ "$backup_type" = "partial" ]
+            then
+                delete_local_old_backup "$2" "$local_age"
+            else
+                echo "Deleting all backup from local($2)"
+                find "$2" -type f -delete
+            fi 
+        fi
+    elif [ "$1" = "fs" ]
+    then
+        dirname="$(basename $3)"
+        fsname="$dirname-$(date +%Y-%m-%d).tar.gz"
+        echo "Backuping filestore $dirname to $2"
+        sudo tar zcf "$2/$fsname" "$3"
+        if [[ $? -ne 0 ]]
+        then
+            echo "Backup failed! Failed to backup filestore $3"
             send_errormail "Backup failed! Failed to backup filestore $3"
             return 1
         fi
+        if [ "$backup_type" = "sync" ]
+        then
+            echo "Saving backup to daily backup directory"
+            mv "$2/$fsname" "$2/fs/daily/"
+            echo "Deleting 7 days old backup from daily backup"
+            delete_local_old_backup "$2/fs/daily/" 7
+            if [ "$today_day" = "Monday" ]
+            then
+                echo "Saving backup to weekly backup directory"
+                cp "$2/fs/daily/$fsname" "$2/fs/weekly/"
+                echo "Deleteing 30 days old backup from weekly backup directory"
+                delete_local_old_backup "$2/fs/weekly/" 30
+            fi
+            if [ "$today_date" = "28" ]
+            then
+                echo "Saving backup to monthly backup directory"
+                cp "$2/fs/daily/$fsname" "$2/fs/monthly/"
+                echo "Deleting 90 days old backup from monthly backup directory"
+                delete_local_old_backup "$2/fs/monthly/"
+            fi
+            cloud_sync "fs"
+        else
+            cloud_upload "$2/$fsname" "fs/daily" 6
+            if [ "$today_day" = "Monday" ]
+            then
+                cloud_upload "$2/$fsname" "fs/weekly" 29
+            fi
+            if [ "$today_date" = "28" ]
+            then
+                cloud_upload "$2/$fsname" "fs/monthly" 89
+            fi
+            if [ "$backup_type" = "partial" ]
+            then
+                delete_local_old_backup "$2" "$local_age"
+            else
+                echo "Deleting all backup from local($2)"
+                find "$2" -type f -delete
+            fi 
+        fi
     else
-        echo ""
+        echo "Error! Wrong type!"
+        exit 1
+    fi
 }
 
-delete_s3_old_backup() {
-    aws s3 --endpoint-url $s3_endpoint ls $s3_bucket | grep " DIR " -v | while read -r line;
-    do
-        createDate=$(echo $line | awk {'print $1" "$2'})
-        createDate=$(date -d "$createDate" +%s)
-        olderThan=$(date -d "$2 day ago" +%s)
-        if [[ $createDate -lt $olderThan ]]
-        then
-            fileName=`echo $line | awk {'print $4'}`
-            if [[ $fileName != "" ]]
-            then
-                printf 'Deleting "%s"\n' $fileName
-                aws s3 --endpoint-url $s3_endpoint rm $s3_bucket/$1
-            fi
-        fi
-    done    
-}
+################################################################################# Start backup action
+
+## Create directory if not exist, just in case
+# only if backup_type="sync"
+if [ "$backup_type" = "sync" ]
+then
+    mkdir -p $backup_location/{db,fs}/{daily,weekly,monthly}
+fi
+
+## Check database, exit if not exists
+# if db not found then there's no point to try to backup filestore
+check_db="$(sudo -u postgres psql -l | grep "$db_name" | awk {'print $1'})"
+if [ -z $check_db ]
+then
+    echo "Backup failed, database $db_name not found!! Aborting"
+    send_errormail "Backup failed, database $db_name not found!! Aborting"
+    exit 1
+fi
+
+## BACKUP!!!!
+
+backup_function db $backup_location $db_name 
+# if backup database error then no backup filestore 
+# exit if backup db error
+if [[ $? -ne 0 ]]
+then 
+    exit 1
+fi
+
+# backup databse success, then backup filestore
+backup_function fs $backup_location $fs_location
